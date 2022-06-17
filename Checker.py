e44c49eaaa292e47f26eb4fe4799a1d03d665073
uvjour <uvjour@gmail.com>
2022-06-16 22:14:05 -0400

    update

--- Checker.py
+++ Checker.py
@@ -1,11 +1,20 @@
 import requests
 import hashlib
 
-def request_api_result(password):
+def request_api_result(get_char):
     url = 'https://api.pwnedpasswords.com/range' + 'pass'
     res = requests.get(url)
-    print(res)
+    return res
 
 def pwned_api_check(password):
-    pass
+    hashPassword = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    initial5 = hashPassword[:5]
+    restPassword = hashPassword[5:]
+    response = request_api_result(initial5)
+    
+    return response
+
+
+
+
 

528665aaeec8fec47a525ea2343ccafcb9cd9213
uvjour <uvjour@gmail.com>
2022-06-16 22:05:22 -0400

    add check

--- Checker.py
+++ Checker.py
@@ -1,6 +1,11 @@
 import requests
 import hashlib
 
-url = 'https://api.pwnedpasswords.com/range', 'pass'
-res = requests.get(url)
-print(res)
+def request_api_result(password):
+    url = 'https://api.pwnedpasswords.com/range' + 'pass'
+    res = requests.get(url)
+    print(res)
+
+def pwned_api_check(password):
+    pass
+
